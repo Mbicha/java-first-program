f773da23834ce58efd6f241aaf73cd5577817292
Mbithi <mbithicharlse@gmail.com>
2020-09-28 20:55:41 +0300

    Completed module

--- src/main/java/com/h2/Utilities.java
+++ src/main/java/com/h2/Utilities.java
@@ -1,39 +1,34 @@
 package com.h2;
 
 public class Utilities {
-    public static long getLongValue(String in){
+
+    public static long getLongValue(String in) {
         long out = Long.MIN_VALUE;
-        try{
+        try {
             out = Long.parseLong(in);
-
-        }catch (NumberFormatException e){
-            new IllegalAccessException(in + " cannot be converted into a 'long' value. Exiting program.");
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(in + " cannot be converted into a 'long' value. Exiting program.");
         }
         return out;
     }
 
-    public static int getIntValue(String in){
+    public static int getIntValue(String in) {
         int out = Integer.MIN_VALUE;
-        try{
+        try {
             out = Integer.parseInt(in);
-
-        }catch (NumberFormatException e){
-            new IllegalAccessException(in + " cannot be converted into a 'int' value. Exiting program.");
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(in + " cannot be converted into a 'int' value. Exiting program.");
         }
         return out;
     }
 
-    public static float getFloatValue(String in){
+    public static float getFloatValue(String in) {
         float out = Float.MIN_VALUE;
-        try{
+        try {
             out = Float.parseFloat(in);
-
-        }catch (NumberFormatException e){
-            new IllegalAccessException(in + " cannot be converted into a 'float' value. Exiting program.");
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(in + " cannot be converted into a 'float' value. Exiting program.");
         }
         return out;
     }
-
-
-    //getLongValue()
 }
